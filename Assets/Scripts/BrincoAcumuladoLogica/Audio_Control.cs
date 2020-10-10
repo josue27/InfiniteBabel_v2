19d56b83d3032b1afa44776390387eab7dbd15fa
josue27 <iosue27@gmail.com>
2020-10-09 21:05:39 -0500

    UIX update
    -Se agrego botones graficos de Audio y Settings
    -Se agrego panel de nombre de personajes

--- Assets/Scripts/BrincoAcumuladoLogica/Audio_Control.cs
+++ Assets/Scripts/BrincoAcumuladoLogica/Audio_Control.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 public class Audio_Control : MonoBehaviour
 {
-
+    public AudioMixer masterMixer;
     public Button musica_boton;
     public Slider musicaSlider;
     public AudioSource musicaSource;
@@ -12,31 +13,70 @@ public class Audio_Control : MonoBehaviour
     public Slider sfxSlider;
     public AudioSource sfxSoure;
 
-    private void Awake() {
+    const string MUSICA_ID = "musicaVolumen";
+    const string SFX_ID = "sfxVolumen";
+
+    private void Awake()
+    {
         musicaSlider.onValueChanged.AddListener(SliderMusica);
         sfxSlider.onValueChanged.AddListener(SliderSFX);
-    }
-    public void Musica()
-    {
-        if(!musicaSource) {Debug.Log("No hay source de musica"); return;}
 
-        musicaSource.volume = 0.0f;
-    }
-    public void SFX()
-    {
-        if(!sfxSoure) {Debug.Log("No hay source de sfx"); return;}
+        masterMixer.GetFloat(MUSICA_ID, out float v);
+        musicaSlider.value = 1f;
+        sfxSlider.value = 1f;
 
-        sfxSoure.volume = 0.0f;
+
     }
+
+    public void ToggleMusica()
+    {
+      
+
+        if(musicaSlider.value <= musicaSlider.minValue)
+        {
+            SliderMusica(musicaSlider.maxValue);
+            musicaSlider.value = musicaSlider.maxValue;
+            return;
+        }
+        else
+        {
+            SliderMusica(musicaSlider.minValue);
+            musicaSlider.value = musicaSlider.minValue;
+
+
+        }
+    }
+
+    public void ToggleSFX()
+    {
+
+
+        if (sfxSlider.value <= sfxSlider.minValue)
+        {
+            SliderSFX(sfxSlider.maxValue);
+            sfxSlider.value = sfxSlider.maxValue;
+
+        }
+        else
+        {
+            SliderSFX(sfxSlider.minValue);
+            sfxSlider.value = sfxSlider.minValue;
+
+
+        }
+    }
+ 
 
     public void SliderMusica(float v)
     {
-        musicaSource.volume = v;
+        //musicaSource.volume = v;
+        masterMixer.SetFloat(MUSICA_ID, Mathf.Log(v)*20);
         Debug.Log($"Musica: {v}");
     }
     public void SliderSFX(float v)
     {
         sfxSoure.volume = v;
+        masterMixer.SetFloat(SFX_ID, Mathf.Log( v)*20);
 
         Debug.Log($"SFX: {v}");
         
---
+++ Assets/Scripts/BrincoAcumuladoLogica/SettingsControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsControl : MonoBehaviour
+{
+    public GameObject panelSettings;
+
+
+
+    public void Toggle_PanelSettings()
+    {
+        if (!panelSettings) return;
+
+        panelSettings.SetActive(!panelSettings.activeInHierarchy);
+    }
+        
+     
+}
